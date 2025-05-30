ca7dd93b2dc860b467b7d37681f8d2716e5fe2c1
Ricelli Carvalho <ricelli.martinsdecarvalho@gmail.com>
2025-05-30 16:44:21 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-const Home = () => {
-  return (
-    <div>
-      <h1 className="text-red-500">Home</h1>
-    </div>
-  );
-};
+import { Button } from "@/components/ui/button";
 
-export default Home;
+export default function Home() {
+  return <Button>Click me</Button>;
+}
